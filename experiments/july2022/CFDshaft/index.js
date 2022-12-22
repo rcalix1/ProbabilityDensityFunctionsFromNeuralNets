3a197ad2de452305afe1ac7c6c7b3726f6f3a37d
Ricardo Calix <rcalix1@gmail.com>
2022-12-21 18:14:41 -0600

    Add files via upload

---
+++ experiments/july2022/CFDshaft/index.js
@@ -0,0 +1,55 @@
+
+
+
+
+async function runExample() {
+    
+
+  // Create an ONNX inference session with default backend.
+  const session = new onnx.InferenceSession();
+ 
+  await session.loadModel("./CFDshaftSIO_mlp.onnx");
+
+  var x = new Float32Array(1, 5);
+
+  //x = [   38.0000,     0.0000,   300.0000,    27.0000, 308750.3750,    12.1540, 359906.3125,  1459.8170, 24494.0000, 87618.0156]
+  
+
+  var x = [];
+  x[0] = document.getElementById('box0').value;
+  x[1] = document.getElementById('box1').value;
+  x[2] = document.getElementById('box2').value;
+  x[3] = document.getElementById('box3').value;
+  x[4] = document.getElementById('box4').value;
+ 
+
+  
+  const tensorX = new onnx.Tensor(x, 'float32', [1, 5]);
+  
+   
+  
+  const outputMap = await session.run([tensorX]);
+  const outputData = outputMap.get('output1');
+  
+  
+ 
+
+  // PREDS DIV 
+  const predictions = document.getElementById('predictions');
+  
+ 
+
+
+  predictions.innerHTML = `<hr> Got an output Tensor of size ${outputData.data.length} with values being: <br/> 
+  <table>
+  <tr><td> o_shaft_co_utiliz</td><td> ${outputData.data[0]} </td></tr>
+  <tr><td> o_shaft_h2_utiliz</td>  <td> ${outputData.data[1]} </td></tr> 
+  <tr><td> o_shaft_top_gas_temp_c</td><td> ${outputData.data[2]} </td></tr>
+  <tr><td> o_shaft_press_drop_pa</td>   <td> ${outputData.data[3]} </td></tr> 
+  <tr><td> o_shaft_coke_rate_kg_thm</td> <td> ${outputData.data[4]} </td></tr> 
+ </table>   `;
+ 
+
+
+
+}
